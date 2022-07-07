ae08d54ea6b2e3c028d0f68fe7f90386b56400d1
TRAN QUANG HUYEN <huyenquangtran2002@gmail.com>
2022-07-07 15:24:12 +0700

    new

--- hello.py
+++ hello.py
@@ -35,11 +35,7 @@ def home_page():
 
 @app.route('/market')
 def market_page():
-    items = [
-        {'id': 1, 'name': 'Phone', 'barcode': '893212299897', 'price': 500},
-        {'id': 2, 'name': 'Laptop', 'barcode': '123985473165', 'price': 900},
-        {'id': 3, 'name': 'Keyboard', 'barcode': '231985128446', 'price': 150}
-    ]
+    items = Item.query.all()
     return render_template('flask_market/market.html', item_name=items)
 @app.route('/login_page')
 def login():
