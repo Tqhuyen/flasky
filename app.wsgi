1f808d3310edf0a249f67f09fdbe9b00bbb93bf2
TRAN QUANG HUYEN <huyenquangtran2002@gmail.com>
2022-07-07 23:50:05 +0700

    add database

--- app.wsgi
+++
@@ -1,5 +0,0 @@
-import sys
-
-sys.path.insert(0, "/var/www/html/flasky")
-
-from hello import app as application
--- flask_market/routes.py
+++ flask_market/routes.py
@@ -3,18 +3,6 @@ from flask import render_template
 from flask import request
 from flask_market.model import Item
 
-@app.route('/')
-@app.route('/client_in4')
-def client():
-    user_agent = request.headers.get('User-Agent')
-    return f"<h1>Your browser agent is {user_agent}</h1>"
-@app.route('/facebook')
-def facebook():
-    return render_template('index.html')
-@app.route('/<user>')
-def user(user):
-    return render_template('user.html', user=user)
-
 # ------------------------------------------------Market App--------------------------------------------
 
 @app.route('/home')
@@ -32,6 +20,3 @@ def login():
 @app.route('/register_page')
 def register():
     return
-
-
-# --------------------------------------------Browser Infomation--------------------------------------
