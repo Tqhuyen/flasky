d1f16ae9f508e922c0b13f41c114097f3a0a9f6e
TRAN QUANG HUYEN <huyenquangtran2002@gmail.com>
2022-07-08 14:56:00 +0700

    add login directory

--- shoppe_fake/routes.py
+++ shoppe_fake/routes.py
@@ -1,3 +1,4 @@
+from flask_market.routes import login
 from shoppe_fake import app_shoppe
 from flask import render_template
 
@@ -5,3 +6,6 @@ from flask import render_template
 @app_shoppe.route('/shoppe')
 def shoppe_index():
     return render_template('index_shoppe.html')
+@app_shoppe.route('/shoppe/login')
+def shoppe_login():
+    return render_template('login.html')
