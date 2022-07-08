1bdcf79ca43e966bfc3a5ce268282aea80fc2930
TRAN QUANG HUYEN <huyenquangtran2002@gmail.com>
2022-07-08 14:50:14 +0700

    login

--- run.py
+++ run.py
@@ -2,4 +2,4 @@ from flask_market import app
 from shoppe_fake import app_shoppe
 
 if __name__ == '__main__':
-    app.run(host='0.0.0.0',debug=True)
+    app_shoppe.run(host='0.0.0.0',port=80,debug=True)
